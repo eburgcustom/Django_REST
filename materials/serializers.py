e20e8fed0236e29dd82ad7675a2fb3ccc0c796ca
eburgcustom <gif8686@gmail.com>
2026-02-21 18:18:19 +0500

    A lesson output field has been implemented for the course model

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,22 +2,6 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор для модели Course.
-    Преобразует данные курса в JSON и обратно.
-    """
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-    def get_lessons_count(self, obj):
-        """Возвращает количество уроков в курсе."""
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     """
     Сериализатор для модели Lesson.
@@ -26,3 +10,20 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Course.
+    Преобразует данные курса в JSON и обратно.
+    """
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def get_lessons_count(self, obj):
+        """Возвращает количество уроков в курсе."""
+        return obj.lessons.count()
